a9629b81e2517883fe8925d3e885118919507868
Magno Correa <magno.correa@accenture.com>
2020-11-13 15:46:36 -0300

    v

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,21 @@
-function Home() {
-    return <div>Olá Mundo!!!</div>
+import React, { Component } from "react";
+
+import "tabler-react/dist/Tabler.css";
+
+import { Card, Button } from "tabler-react";
+
+class Home extends Component {
+  render() {
+    return (
+      <Card>
+        <Card.Header>
+          <Card.Title>Card Title</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Button color="primary">A Button</Button>
+        </Card.Body>
+      </Card>
+    );
+  }
 }
 export default Home;
